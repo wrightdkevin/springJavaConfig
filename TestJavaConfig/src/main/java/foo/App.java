b96c86e202a8e6d4d08cc253096bc3c6d75cb1a4
kevin wright <kevin.wright@fdmgroup.com>
2016-02-10 10:13:13 +0000

    dummy change

--- TestJavaConfig/src/main/java/foo/App.java
+++ TestJavaConfig/src/main/java/foo/App.java
@@ -9,6 +9,8 @@ public class App {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("start");
+		
 		 ApplicationContext ctx = 
 				   new AnnotationConfigApplicationContext(HelloWorldConfig.class);
 				   
